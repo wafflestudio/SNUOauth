14eb96f1cbef96b8fdebf3f9cc529f946c79ae10
Taekmin Kim <tantara.tm@gmail.com>
2013-07-28 23:06:58 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SNUOauth::Application.config.secret_key_base = '8d64d9f2f80e6b9b17c375ee78bae08ce737a1c6ebe453ed93e3f058f671c39639f997ebeb494811aeb3bc0091cfe25aa71923058e6aa3abb72769b5ea5eed64'
